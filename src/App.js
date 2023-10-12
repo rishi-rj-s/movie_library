a71816bb9c071085613bf9b27eb352fb27fdf8eb
rishi-rj-s <rishisanu007@example.com>
2023-10-12 18:41:54 +0530

    undo if any wrong

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App=()=>{
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
 
-    const searchMovies= async (title)=>{
-        const response = await fetch(`${API_URL}&s=${title}`);
+    const searchMovies= async (title) => {
+        const response = await fetch(`${API_URL} &s=${title}`);
         const data = await response.json();
 
         setMovies(data.Search);
